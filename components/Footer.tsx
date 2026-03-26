631208e6b07c2e794578fd63923307d9747d982d
Zenex11 <122437746+Zenex11@users.noreply.github.com>
2026-03-26 21:15:13 +0200

    Add Footer component with copyright notice

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-900 text-white text-center py-4 mt-10">
+      <p>© 2026 Club. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
